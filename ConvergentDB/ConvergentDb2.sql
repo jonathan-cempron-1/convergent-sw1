1ea6cd506dd4cdc9cf8f6515cf30b4ed3d5115e0
jonats <jp.cempron@gmail.com>
2018-01-27 02:24:25 +0800

    optimized new database

---
+++ ConvergentDB/ConvergentDb2.sql
@@ -0,0 +1,324 @@
+CREATE DATABASE  IF NOT EXISTS `convergentDb2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `convergentDb2`;
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: convergentDb2
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB-0+deb9u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Account`
+--
+
+DROP TABLE IF EXISTS `Account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Account` (
+  `idAccount` int(11) NOT NULL AUTO_INCREMENT,
+  `idBank` int(11) NOT NULL,
+  `dateStart` date DEFAULT NULL,
+  `dateEnd` date DEFAULT NULL,
+  PRIMARY KEY (`idAccount`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Account`
+--
+
+LOCK TABLES `Account` WRITE;
+/*!40000 ALTER TABLE `Account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `AccountStatus`
+--
+
+DROP TABLE IF EXISTS `AccountStatus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AccountStatus` (
+  `idAccountStatus` int(11) NOT NULL AUTO_INCREMENT,
+  `fvStatus` varchar(45) DEFAULT NULL,
+  `remarks` varchar(45) DEFAULT NULL,
+  `actionPlan` varchar(45) DEFAULT NULL,
+  `dateSaved` date NOT NULL,
+  `idAccount` int(11) NOT NULL,
+  `idEmployee` int(11) NOT NULL,
+  PRIMARY KEY (`idAccountStatus`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `AccountStatus`
+--
+
+LOCK TABLES `AccountStatus` WRITE;
+/*!40000 ALTER TABLE `AccountStatus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `AccountStatus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Address`
+--
+
+DROP TABLE IF EXISTS `Address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Address` (
+  `idAddress` int(11) NOT NULL AUTO_INCREMENT,
+  `completeAddress` varchar(45) NOT NULL,
+  `zipCode` varchar(45) DEFAULT NULL,
+  `dateSaved` date DEFAULT NULL,
+  `isActive` tinyint(4) DEFAULT NULL,
+  `idLocation` int(11) DEFAULT NULL,
+  `idOwner` int(11) NOT NULL,
+  PRIMARY KEY (`idAddress`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Address`
+--
+
+LOCK TABLES `Address` WRITE;
+/*!40000 ALTER TABLE `Address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Assignment`
+--
+
+DROP TABLE IF EXISTS `Assignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Assignment` (
+  `idAssignment` int(11) NOT NULL AUTO_INCREMENT,
+  `idAccount` varchar(45) NOT NULL,
+  `idEmployee` varchar(45) NOT NULL,
+  `dateStart` date NOT NULL,
+  `dateEnd` date DEFAULT NULL,
+  PRIMARY KEY (`idAssignment`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Assignment`
+--
+
+LOCK TABLES `Assignment` WRITE;
+/*!40000 ALTER TABLE `Assignment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Assignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Bank`
+--
+
+DROP TABLE IF EXISTS `Bank`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Bank` (
+  `idBank` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `branch` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idBank`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Bank`
+--
+
+LOCK TABLES `Bank` WRITE;
+/*!40000 ALTER TABLE `Bank` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Bank` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ContactInfo`
+--
+
+DROP TABLE IF EXISTS `ContactInfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ContactInfo` (
+  `idContactInfo` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) DEFAULT NULL,
+  `contactInfo` varchar(45) NOT NULL,
+  `dateSaved` date NOT NULL,
+  `isActive` tinyint(4) NOT NULL DEFAULT '1',
+  `idOwner` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idContactInfo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ContactInfo`
+--
+
+LOCK TABLES `ContactInfo` WRITE;
+/*!40000 ALTER TABLE `ContactInfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ContactInfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Employee`
+--
+
+DROP TABLE IF EXISTS `Employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Employee` (
+  `idEmployee` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `adminPrivilege` tinyint(4) DEFAULT '0',
+  `gender` varchar(45) DEFAULT NULL,
+  `bday` date DEFAULT NULL,
+  PRIMARY KEY (`idEmployee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Employee`
+--
+
+LOCK TABLES `Employee` WRITE;
+/*!40000 ALTER TABLE `Employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Location`
+--
+
+DROP TABLE IF EXISTS `Location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Location` (
+  `idLocation` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `longitude` double NOT NULL,
+  `latitude` double NOT NULL,
+  PRIMARY KEY (`idLocation`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Location`
+--
+
+LOCK TABLES `Location` WRITE;
+/*!40000 ALTER TABLE `Location` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `PersonInterest`
+--
+
+DROP TABLE IF EXISTS `PersonInterest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PersonInterest` (
+  `idPersonInterest` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `bday` date DEFAULT NULL,
+  `idAccount` varchar(45) NOT NULL,
+  PRIMARY KEY (`idPersonInterest`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PersonInterest`
+--
+
+LOCK TABLES `PersonInterest` WRITE;
+/*!40000 ALTER TABLE `PersonInterest` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PersonInterest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ResourcePerson`
+--
+
+DROP TABLE IF EXISTS `ResourcePerson`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ResourcePerson` (
+  `idResourcePerson` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `relationship` varchar(45) DEFAULT NULL,
+  `idPersonInterest` varchar(45) NOT NULL,
+  PRIMARY KEY (`idResourcePerson`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ResourcePerson`
+--
+
+LOCK TABLES `ResourcePerson` WRITE;
+/*!40000 ALTER TABLE `ResourcePerson` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ResourcePerson` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Vehicle`
+--
+
+DROP TABLE IF EXISTS `Vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Vehicle` (
+  `idVehicle` int(11) NOT NULL AUTO_INCREMENT,
+  `numberPlate` varchar(45) NOT NULL,
+  `make` varchar(45) NOT NULL,
+  `year` int(11) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `color` varchar(45) NOT NULL,
+  `chassisNum` varchar(45) DEFAULT NULL,
+  `engineNum` varchar(45) DEFAULT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  `otherInfo` varchar(45) DEFAULT NULL,
+  `idAccount` int(11) NOT NULL,
+  PRIMARY KEY (`idVehicle`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Vehicle`
+--
+
+LOCK TABLES `Vehicle` WRITE;
+/*!40000 ALTER TABLE `Vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-27  2:24:16
